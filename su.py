5cbabb4baa995445e320be7f98cf47227a696030
sudeep-25 <sudeeppoojary32@gmail.com>
2025-07-04 12:04:45 +0530

    I have changes

---
+++ su.py
@@ -0,0 +1,21 @@
+class Fan:
+    def __init__(self):
+        self.brand = "BaJaj"
+        self.color = "black"
+        self.cost = "2000"
+        self.noOfBlades = 3
+    
+    def on(self):
+        print("fan is on")
+    def rotate(self):
+        print("fan is rotating")
+
+f1 = Fan()
+
+print(f1.brand)
+print(f1.color)
+print(f1.cost)
+print(f1.noOfBlades)
+
+f1.on()
+f1.rotate()
