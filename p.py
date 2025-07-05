ebc56860d51c5847a252c81a43f0d33938dae770
sudeep-25 <sudeeppoojary32@gmail.com>
2025-07-05 12:16:00 +0530

    conditional statement

---
+++ p.py
@@ -0,0 +1,14 @@
+print("Enter a value1")
+
+a=int(input())
+
+print("Enter a value2")
+
+b=int(input())
+
+if(a>b):
+    print("a is great")
+elif(b>a):
+    print("b is great")
+else:
+    print("a==b")
