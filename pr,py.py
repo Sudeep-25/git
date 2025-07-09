5203b54046f62f92c202acfe9b61e467bbf39f12
sudeep-25 <sudeeppoojary32@gmail.com>
2025-07-09 18:01:39 +0530

    pr,py.py

---
+++ pr,py.py
@@ -0,0 +1,42 @@
+print(5+3+2) # addition
+
+print(88.5+99.2) #addition
+
+print(6+2+3+5+2.3+0) # addition
+
+print(+9) #positive number
+
+print(97.6)# positive number
+
+
+print(99-2-55-3)  #subtraction
+print(31-20)#subtraction
+print(100.3-33.2-0.2)#subtraction
+print(-92.3)#negative number
+print(-0.3)#negative number
+
+
+print(10*60*3) #mutiplication
+print(2.3*8.5) #mutiplication
+print(2.3*7)#mutiplication
+
+
+
+print(2**3) # 2*2*2=8
+print(3**2) # 3*3=9
+print(3.2**2)
+
+
+print(10//5)#2 floor div[//]
+print(22//7)#3
+
+print(18/2) #float div[/]
+print(25/3)
+print(41/5)
+
+print(10%2) # modulous[%]
+print(10%3)
+print(3%100)
+print(2%200)
+print(5%888)
+print(11%10258)
